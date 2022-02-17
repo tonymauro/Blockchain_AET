9f65b4b171d3ef36ede6cd3a277911bef1234e2d
AVDestroyer <mr.arnav.vora@gmail.com>
2022-02-17 14:20:17 -0800

    Python transaction script

--- transaction.py
+++ transaction.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 accounts = w3.eth.accounts
-w3.personal.unlockAccount(accounts[0],"password1",15000)
+w3.geth.personal.unlockAccount(accounts[0],"password1",15000)
 print(accounts)
 print(w3.eth.get_balance(accounts[0]))
 print(w3.eth.get_balance(accounts[1]))
