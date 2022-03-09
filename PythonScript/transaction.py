9f77983166ab1f9495329708181de82f3f2f2029
AVDestroyer <mr.arnav.vora@gmail.com>
2022-03-09 14:31:18 -0800

    Updated python script to use user input

--- PythonScript/transaction.py
+++ PythonScript/transaction.py
@@ -1,3 +1,28 @@
+#Make sure that the user data for two accounts is in the correct format
+def validate(lst):
+	if (len(lst)!=2):
+		return False
+	for i in lst:
+		if (not i.isnumeric()):
+			return False
+		elif (int(i)==0):
+			return False
+	return True
+
+#User input for accounts
+a1 = 0
+a2 = 0
+accountInput = ""
+while (not validate(accountInput)):
+	accountInput = input("Enter two accounts in the format '<from> <to>' to send a transaction between. Use account numbers that are above 0").split()
+a1,a2 = map(int,accountInput)
+
+#User input for transaction value
+ethInput = ""
+while (not ethInput.isnumeric())"
+	ethInput = input("Enter the value of the transaction in ETH")
+ethTransact = int(ethInput)
+
 #import web3 and connect to localhost over HTTP
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
@@ -10,11 +35,11 @@ w3.geth.personal.unlockAccount(accounts[0],"password1",15000)
 
 #prints accounts and balances
 print(accounts)
-print(w3.eth.get_balance(accounts[0]))
-print(w3.eth.get_balance(accounts[1]))
+print(w3.eth.get_balance(accounts[a0]))
+print(w3.eth.get_balance(accounts[a1]))
 
 #creates transaction
-tx = {'from':accounts[0], 'to':accounts[1],'value':w3.toWei(0.5,"ether")}
+tx = {'from':accounts[a0], 'to':accounts[a1],'value':w3.toWei(ethTransact,"ether")}
 
 #sends transaction and saves the hash
 thash = w3.eth.send_transaction(tx)
@@ -25,5 +50,5 @@ w3.eth.wait_for_transaction_receipt(thash)
 w3.geth.miner.stop()
 
 #Print new account balances. Currently, mining a block gives you 2 ETH, so it looks like the balance of both accounts went up
-print(w3.eth.get_balance(accounts[0]))
-print(w3.eth.get_balance(accounts[1]))
+print(w3.eth.get_balance(accounts[a1]))
+print(w3.eth.get_balance(accounts[a2]))
